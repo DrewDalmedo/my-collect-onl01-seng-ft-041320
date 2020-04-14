f260e0c510326c95fa49014178dfac545abca6ba
DrewDalmedo <andrewdalmedo@gmail.com>
2020-04-14 16:05:50 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
+def my_collect(array)
+    modified_array = []
+    
+    index = 0
+    while index < array.size
+        modified_array.push(yield array[index])
+        index += 1
+    end
 
+    return modified_array
+end
 
